b37014672aad287a9848279f1486ceb47dc7cb0c
Alex <77007088+AlexEkdahl@users.noreply.github.com>
2023-03-01 22:33:25 +0100

    tried to remove client dep and make server a telnet server

--- go.mod
+++ go.mod
@@ -6,10 +6,11 @@ require github.com/google/uuid v1.3.0
 
 require (
 	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
+	golang.org/x/sys v0.5.0
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
 	github.com/google/go-cmp v0.5.9 // indirect
-	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
 )
---
+++ pkg/network/telnetserver.go
@@ -0,0 +1,128 @@
+package network
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"sync"
+
+	"github.com/AlexEkdahl/snakes/pkg/game"
+)
+
+// type MessagePrinter interface {
+// 	PrintMessage(msg string)
+// 	Clear()
+// }
+
+type TelnetServer struct {
+	game    *game.Game
+	conn    net.Listener
+	Printer MessagePrinter
+	clients map[net.Conn]struct{}
+	mutex   sync.Mutex
+}
+
+func NewTelnetServer(port string, g *game.Game) (*TelnetServer, error) {
+	c, err := net.Listen("tcp", port)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Server started and listening on", port)
+
+	return &TelnetServer{
+		game:    g,
+		conn:    c,
+		Printer: NewPrinter(),
+		clients: make(map[net.Conn]struct{}),
+	}, nil
+}
+
+func (s *TelnetServer) Start() {
+	// Start a goroutine to listen to the GameStateChan
+	go s.game.Start()
+	go s.handleConnections()
+	go s.broadcastGameState()
+}
+
+func (s *TelnetServer) broadcastGameState() {
+	for gameState := range s.game.GameStateChan {
+		s.mutex.Lock()
+		for c := range s.clients {
+			clearScreen := []byte{27, 91, 72, 27, 91, 74}
+			message := append(clearScreen, *gameState...) // Send game state to client
+			if _, err := c.Write(message); err != nil {
+				fmt.Printf("Error sending game state to client: %v\n", err)
+			}
+		}
+		s.mutex.Unlock()
+	}
+}
+
+func (s *TelnetServer) handleConnections() {
+	for {
+		conn, err := s.conn.Accept()
+		if err != nil {
+			continue
+		}
+
+		fmt.Printf("New client connected: %s\n", conn.RemoteAddr())
+
+		s.mutex.Lock()
+		s.clients[conn] = struct{}{}
+		p := game.NewPlayer(conn)
+		s.game.Players = append(s.game.Players, p)
+		s.mutex.Unlock()
+
+		go s.handleClient(*p)
+	}
+}
+
+func (s *TelnetServer) handleClient(p game.Player) {
+	defer func() {
+		s.mutex.Lock()
+		delete(s.clients, p.Conn)
+		s.mutex.Unlock()
+		p.Conn.Close()
+	}()
+
+	// Get the file descriptor for the connection
+
+	// Negotiate Telnet options
+	if _, err := p.Conn.Write([]byte{255, 253, 34, 255, 250, 34, 1, 0, 255, 240, 255, 251, 1}); err != nil {
+		fmt.Printf("Error negotiating Telnet options: %v\n", err)
+		return
+	}
+
+	reader := bufio.NewReader(p.Conn)
+	for {
+		b, err := reader.ReadByte()
+		if err != nil {
+			break
+		}
+
+		input := string(b)
+		switch input {
+		case "h":
+			s.game.InputChan <- game.InputMessage{Input: game.Left, PlayerID: p.ID}
+		case "j":
+			s.game.InputChan <- game.InputMessage{Input: game.Down, PlayerID: p.ID}
+		case "k":
+			s.game.InputChan <- game.InputMessage{Input: game.Up, PlayerID: p.ID}
+		case "l":
+			s.game.InputChan <- game.InputMessage{Input: game.Right, PlayerID: p.ID}
+		case "q":
+			p.Conn.Close()
+			delete(s.clients, p.Conn)
+			s.game.RemovePlayer(p.ID)
+
+			fmt.Printf("Player %v disconnected\n", p.ID)
+		default:
+			continue
+		}
+	}
+}
+
+func (s *TelnetServer) Stop() {
+	s.game.Stop()
+	s.conn.Close()
+}
